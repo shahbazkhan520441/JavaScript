5a2d927e7bc5dfede3bae418120440b0f480a6bc
shahbazkhan520441 <shahbazkhan520441@gmail.com>
2024-10-22 23:11:32 +0530

    started practice

---
+++ Style.js
@@ -0,0 +1,33 @@
+
+console.log("one")
+// alert("are you adult or not ")
+// let task=confirm(" task is complted or not")
+// console.log(task)
+// if(task){
+//     console.log(" hey you can live the class")
+//     alert("hey  you can live the class")
+//     document.write("hello how are you")
+    
+// }else{
+//     console.log("hey do the task first hten go")
+//     alert("hey you have not completed your task")
+// }
+
+// // to print on the browser
+// document.write("hello how are you")
+
+
+// // prompt to take the data from user
+
+// let number=Number(prompt("enter your primary mobile number"))
+// console.log(number)
+
+// let number2=Number(prompt("enter you secondery number"))
+// console.log(number2)
+
+// console.log(typeof number)
+// confirm.log(typeof number2)
+
+
+let name=String(prompt("enter your name"))
+console.log(name)
